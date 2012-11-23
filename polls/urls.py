9251792bfa53bbd09612e0f883c3cc8946947cf0
yatinla <mike@taylorwebhome.org>
2012-11-23 06:13:09 -0800

    Moving urls to polls etc.

---
+++ polls/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('polls.views',
+    url(r'^$', 'index'),
+    url(r'^(?P<poll_id>\d+)/$', 'detail'),
+    url(r'^(?P<poll_id>\d+)/detail/$', 'detail'),
+    url(r'^(?P<poll_id>\d+)/results/$', 'results'),
+    url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
+
+)
+
+
+
--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,30 @@
 from django.http import HttpResponse
 from polls.models import Poll
 from django.template import Context, loader
+from django.shortcuts import render_to_response
 
 def index(request):
   latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+  '''
+  This was the hard way:
   t = loader.get_template('polls/index.html')
   c = Context({
       'latest_poll_list': latest_poll_list, })
 
   #output = ', '.join([p.question for p in latest_poll_list])
   return HttpResponse(t.render(c))
+  '''
+  #This is the easy way
+  return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
 
 def detail(request, poll_id):
-  return HttpResponse("You're looking at poll %s." % poll_id )
+  try:
+    p = Poll.objects.get(pk=poll_id)
+  except Poll.DoesNotExist:
+    raise Http404
+  return render_to_response('polls/detail.html', {'poll': p})
+
+  #return HttpResponse("You're looking at poll %s." % poll_id )
 
 def results(request, poll_id):
   return HttpResponse("You're looking at the result of poll %s." % poll_id )
