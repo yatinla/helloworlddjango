5ebad6606b8a3a592d70d9ff4fb4bbcd55ed687a
yatinla <mike@taylorwebhome.org>
2012-11-23 06:20:51 -0800

    Oh well, that didn't work and broke admin

--- hellodjango/urls.py
+++ hellodjango/urls.py
@@ -4,8 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'', include('polls.urls')),
-    url(r'/', include('polls.urls')),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
