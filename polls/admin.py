b88deb0d071251e314434a9d93ff98df38fad2a2
yatinla <mike@taylorwebhome.org>
2012-11-23 03:09:06 -0800

    Adding more stuff to admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,16 @@
-from polls.models import Poll
+from polls.models import Poll, Choice
 from django.contrib import admin
 
-admin.site.register(Poll)
+class ChoiceInline(admin.StackedInline):
+  model = Choice
+  extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+  fieldsets = [
+    (None,                {'fields': ['question']}),
+    ('Date information',  {'fields': ['pub_date']}),
+    ]
+  inlines = [ChoiceInline]
+
+admin.site.register(Poll, PollAdmin)
 
